8c62c0a34396d4b4db36b4fb2d12d63519a06c31
Weijie Ou <145056577+Weijie230@users.noreply.github.com>
2026-05-16 15:10:55 -0400

    Implement text-to-speech functionality in dictionary.js
    Added functions for text-to-speech and sentence synchronization.

---
+++ dictionary.js
@@ -0,0 +1,36 @@
+function speak(textToSay) {
+  const message = new SpeechSynthesisUtterance(textToSay);
+  message.pitch = 1.2;
+  message.rate = 1.0;
+  window.speechSynthesis.speak(message);
+}
+
+function syncSentence() {
+  const menu = document.getElementById("wordSelect");
+  const display = document.getElementById("sentenceDisplay");
+
+  if (menu.selectedIndex > 0) {
+    const selectedOption = menu.options[menu.selectedIndex];
+    display.value = selectedOption.dataset.sentence;
+  } else {
+    display.value = "";
+  }
+}
+
+function speakWord() {
+  const menu = document.getElementById("wordSelect");
+
+  if (menu.selectedIndex > 0) {
+    const selectedOption = menu.options[menu.selectedIndex];
+    speak(selectedOption.value);
+  }
+}
+
+function speakSentence() {
+  const menu = document.getElementById("wordSelect");
+  const display = document.getElementById("sentenceDisplay");
+
+  if (menu.selectedIndex > 0) {
+    speak(display.value);
+  }
+}
